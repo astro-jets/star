166578244df3c3dd4a4bbbcf4d68d7192b77c414
astro-jets <mphatsomtogolo30@gmail.com>
2025-01-03 22:13:15 +0200

    Fixed axios fetch request

--- components/slider/artists/EmblaCarousel.tsx
+++ components/slider/artists/EmblaCarousel.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { , useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { EmblaOptionsType } from 'embla-carousel'
 import Autoplay from 'embla-carousel-autoplay'
 import useEmblaCarousel from 'embla-carousel-react'
