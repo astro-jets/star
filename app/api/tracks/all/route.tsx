0cb328d84ad2e13b1d12e9a75db84e7796e40743
astro-jets <mphatsomtogolo30@gmail.com>
2025-01-13 11:30:12 +0200

    csr 2 ssr 3

--- app/api/tracks/all/route.tsx
+++ app/api/tracks/all/route.tsx
@@ -6,7 +6,14 @@ export async function GET() {
     await dbConnect();
     try {
         const tracks = await Track.find({});
-        return NextResponse.json({ success: true, data: tracks }, { status: 200 });
+
+        const headers = {
+            "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+            "Pragma": "no-cache",
+            "Expires": "0",
+        };
+
+        return NextResponse.json({ success: true, data: tracks }, { status: 200, headers });
     } catch (error) {
         console.log("error tu => ", error)
         return NextResponse.json({ success: false, error }, { status: 400 });
